312fc79900993d01ceb28966bdacf52157ad3f92
chema90 <79546923+chema90@users.noreply.github.com>
2026-08-12 14:13:24 +0200

    Forzar actualización de la versión móvil final

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'radar-pisos-v40-mobile-actions';
+const CACHE = 'radar-pisos-v40-mobile-actions-final';
 const APP_SHELL = ['./', './data/municipalities.json', './data/interest-zones.json', './data/known-zones.json', './data/municipality-zones/girona.geojson', './data/municipality-zones/barcelona.geojson', './sql-wasm.wasm'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
